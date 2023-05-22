46e7d14b5a7c229ca3ae2afd82db77808ec31bb3
Aleksei Vlasov <aleks101989@mail.ru>
2023-05-22 19:40:53 +0800

    model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,15 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF("./world/scene.gltf");
+  return (
+    <primitive
+      object={earth.scene}
+      scale={0.65}
+      // position-y={0}
+      // rotation-y={0}
+    />
+  );
 };
 
 const EarthCanvas = () => {
@@ -14,7 +22,12 @@ const EarthCanvas = () => {
       shadows
       frameloop="demand"
       gl={{ preserveDrawingbuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 55,
+        near: 0.1,
+        far: 100,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -29,4 +42,4 @@ const EarthCanvas = () => {
   );
 };
 
-export default Earth;
+export default EarthCanvas;
