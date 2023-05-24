7a8164eeb304cdf05d4780756526a41d6674d18e
Aleksei Vlasov <aleks101989@mail.ru>
2023-05-24 16:54:51 +0800

    stars change color

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,12 @@
-import { useState, useRef, Suspense } from "react";
+import { useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
+import { useContext } from "react";
+import { ThemeContext } from "../../App";
 
 const Stars = (props) => {
+  const { theme } = useContext(ThemeContext);
   const ref = useRef();
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
   useFrame((state, delta) => {
@@ -15,7 +18,7 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="#f272c8"
+          color={`${theme === "light" ? "blue" : "#f272c8"}`}
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
