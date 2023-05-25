579635f0e0750a1d7c1090be13334fa618eb13e6
Aleksei Vlasov <aleks101989@mail.ru>
2023-05-26 06:59:49 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
 // export default defineConfig({
 //   plugins: [react()],
@@ -16,16 +16,16 @@ import react from "@vitejs/plugin-react";
 //     },
 //   },
 // });
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["i18next"],
-      output: {
-        globals: {
-          i18n: "i18next", // Specify the global variable name for "i18next"
-        },
-      },
-    },
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["i18next"],
+//       output: {
+//         globals: {
+//           i18n: "i18next", // Specify the global variable name for "i18next"
+//         },
+//       },
+//     },
+//   },
+// });
