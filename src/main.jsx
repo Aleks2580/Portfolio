74733a5f7c34fbe9ad81713639587e0c0d824fe2
Aleksei Vlasov <aleks101989@mail.ru>
2023-05-26 06:49:57 +0800

    imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import i18n from "./i18n";
+import i18n from "../src/i18n";
 import { I18nextProvider } from "react-i18next";
 import { BrowserRouter } from "react-router-dom";
 
