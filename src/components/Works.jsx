0087ab392e63a1d183168305a4ff780bc1968771
Aleksei Vlasov <aleks101989@mail.ru>
2023-07-30 14:56:59 +0800

    mistake fixed

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -94,7 +94,7 @@ const Works = () => {
       </motion.div>
       <div className="w-full flex">
         <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
+          variants={fadeInProjects("", "", 0.1, 1)}
           className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
         >
           {t("projects.text")}
