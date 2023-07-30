5fb6386dbe98186cd13b43476af6832233cd02d8
Aleksei Vlasov <aleks101989@mail.ru>
2023-07-30 14:17:57 +0800

    experience dleted

--- src/constants/TranslationsExperience.jsx
+++ src/constants/TranslationsExperience.jsx
@@ -5,21 +5,21 @@ export const useExperiences = () => {
   const { t } = useTranslation();
 
   const experiences = [
-    {
-      title: `${t("experience.title3")}`,
-      company_name: `${t("experience.company_name3")}`,
-      icon: assistant,
-      iconBg: "#aaa6c3",
+    // {
+    //   title: `${t("experience.title3")}`,
+    //   company_name: `${t("experience.company_name3")}`,
+    //   icon: assistant,
+    //   iconBg: "#aaa6c3",
 
-      date: `${t("experience.date3")}`,
-      points: [
-        `${t("experience.point8")}`,
-        `${t("experience.point9")}`,
-        `${t("experience.point10")}`,
-        `${t("experience.point11")}`,
-        `${t("experience.point12")}`,
-      ],
-    },
+    //   date: `${t("experience.date3")}`,
+    //   points: [
+    //     `${t("experience.point8")}`,
+    //     `${t("experience.point9")}`,
+    //     `${t("experience.point10")}`,
+    //     `${t("experience.point11")}`,
+    //     `${t("experience.point12")}`,
+    //   ],
+    // },
     {
       title: `${t("experience.title1")}`,
       company_name: `${t("experience.company_name1")}`,
