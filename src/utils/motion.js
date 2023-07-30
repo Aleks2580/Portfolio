6801642304511c95a786a9d14ad0b28e59eb57a0
Aleksei Vlasov <aleks101989@mail.ru>
2023-07-30 14:41:41 +0800

    fadeIn changed

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -18,10 +18,15 @@ export const textVariant = (delay) => {
 
 export const fadeIn = (direction, type, delay, duration) => {
   return {
+    // hidden: {
+    //   x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+    //   y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+    //   opacity: 0,
+    // },
     hidden: {
-      x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
-      y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
-      opacity: 0,
+      x: direction === "left" ? 50 : direction === "right" ? -50 : 0,
+      y: direction === "up" ? 50 : direction === "down" ? -50 : 0,
+      opacity: 0.5,
     },
     show: {
       x: 0,
